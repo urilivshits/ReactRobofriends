f6cc28756178687caf3df12904ee506cee2664ef
Uri Livshits <uri.livshits@gmail.com>
2020-10-21 20:12:09 +0300

    Updates

---
+++ precache-manifest.a9b3ff5a6cd1aff4637aa6e95fef9473.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d040dce55da1bdb7666c30c3d544d671",
+    "url": "/ReactRobofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/ReactRobofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "565aaf1b4afaee16c71a",
+    "url": "/ReactRobofriends/static/css/main.884cff16.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/ReactRobofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactRobofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "565aaf1b4afaee16c71a",
+    "url": "/ReactRobofriends/static/js/main.d64a2b94.chunk.js"
+  },
+  {
+    "revision": "83582f5bdf9bdb4a020a8c1f287e0428",
+    "url": "/ReactRobofriends/static/js/main.d64a2b94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7460f5b430013dc0acfa",
+    "url": "/ReactRobofriends/static/js/runtime-main.18ee8105.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ReactRobofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
